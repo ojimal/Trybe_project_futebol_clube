0b0f940f2e920f6b3c7e960ffa3660567654de2f
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2023-05-17 14:52:59 +0200

    test: teams endpoint test coverage

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -5,7 +5,8 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import TeamModel from '../database/models/Team.model';
-import { allTeamsMock } from './mocks/teams.mock';
+import TeamsService from '../services/teams.service';
+import { allTeamsMock, oneTeamMock } from './mocks/teams.mock';
 
 import { Response } from 'superagent';
 
@@ -21,7 +22,7 @@ describe('test /teams', () => {
         Sinon.restore();
     });
 
-    describe('Find All Teams', () => {
+    describe('Find All Teams /teams GET endpoint', () => {
         it('returns all teams in the API', async () => {
             Sinon.stub(TeamModel, 'findAll').resolves(allTeamsMock as TeamModel[]); 
 
@@ -30,5 +31,39 @@ describe('test /teams', () => {
             expect(status).to.be.equal(200);
             expect(body).to.be.deep.equal(allTeamsMock);
         })
+        it('returns 404 error if teams not found', async () => {
+            Sinon.stub(TeamModel, 'findAll').resolves(undefined);
+      
+            const response = await chai.request(app).get('/teams');
+      
+            expect(response.status).to.be.eq(404);
+            expect(response.body).to.be.deep.eq({ message: 'Teams not found' });
+          });
     })
+
+    describe('Find by Id /teams/:id GET endpoint', async () => {
+
+        afterEach(() => {
+          Sinon.restore();
+        });
+    
+        it('returns a team', async () => {
+          Sinon.stub(TeamModel, 'findByPk').resolves(oneTeamMock as TeamModel);
+    
+          const response = await chai.request(app).get('/teams/1');
+    
+          expect(response.status).to.be.eq(200);
+          expect(response.body).to.be.deep.eq(oneTeamMock);
+        });
+    
+        it('returns 404 error if team not found', async () => {
+          Sinon.stub(TeamModel, 'findByPk').resolves(null);
+    
+          const response = await chai.request(app).get('/teams/1');
+    
+          expect(response.status).to.be.eq(404);
+          expect(response.body).to.be.deep.eq({ message: 'Team not found' });
+        });
+      }
+    );
 });
